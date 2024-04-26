5fa447784b45e0f068f5c07bd04bb791e450c12d
Jason Ng <hieeu.nguyentrong@gmail.com>
2024-04-26 13:49:16 +0900

    Update jason/docker

--- jason/docker/build_docker.sh
+++ jason/docker/build_docker.sh
@@ -3,3 +3,6 @@ sudo docker build -t pytorch-gpu . -f Dockerfile
 
 # create and run a container from the above image  
 sudo docker run --name pytorch-container --gpus all -it -v $(pwd):/codespace pytorch-gpu 
+
+# exec the docker
+docker exec -it image_id bash
